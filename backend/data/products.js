82295cd43ddd197c9058d76c2c3cffa9666f49ab
Stanley Pethel <stanleypethel@yahoo.com>
2022-01-05 11:40:43 -0500

    created Cart screen with styling, created store and redux components

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,56 +1,56 @@
 const products = [
     {
-      name: "PlayStation 5",
+      name: "Cookware Subscription box",
       imageUrl:
-        "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+        "https://images.unsplash.com/photo-1539183204366-63a0589187ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
       description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
-      price: 499,
+        "Camping Cookware subscription boxes offer the highest quality products on the market. You will either recieve a stainless steel kit or titanium kit that includes mugs, thermos types, pots pans and utinsils to keep your camping adventure risk free of bad gear.",
+      price: 150,
       countInStock: 15,
     },
     {
-      name: "Iphone 12",
+      name: "Shelter Subscription Box",
       imageUrl:
-        "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+        "https://images.unsplash.com/photo-1601134917279-ef70a0a90f18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
+        "Welcome to a new era of Camping shelters. Our Tent selections provide the best protection from all 4 seasons and from buge and mold issues common in tent and shelter gear. We provide shelters with waterproof sealant, canvas, and hot tent options to make your subscription worth the cost.Let the fun begin.",
+      price: 750,
       countInStock: 10,
     },
     {
-      name: "Cannon EOS-1D",
+      name: "Light-Saver Box",
       imageUrl:
-        "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1517771778436-39f5763f5270?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
       description:
-        "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-      price: 1300,
+        "Having good lighting in a adventure can be the most important tool to have in your possession. We offer packages with the best lighing products on the market and rest assured we use only rechargable systems so that you dont have to purchase batteries, Ever!",
+      price: 200,
       countInStock: 5,
     },
     {
-      name: "Amazon Alexa",
+      name: "Pack-Box",
       imageUrl:
-        "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+        "https://images.unsplash.com/photo-1510820534885-68189a100ce7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       description:
-        "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
-      price: 50,
+        "You cannot go on an adventure without having the proper storage pack to carry your much needed gear. We have the best packs based on market research and field testing that are sure to help you carry all your goods on your getaway to paradise.",
+      price: 95,
       countInStock: 25,
     },
     {
-      name: "Audio Technica Headphones",
+      name: "Headphones-box",
       imageUrl:
         "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
       description:
-        "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-      price: 233,
-      countInStock: 4,
+        "If you are sitting down by the fire or trying to block out the city noise, grab this box for noise cancelling headphones that are sure to put you in a different mood.",
+      price: 175,
+      countInStock: 9,
     },
     {
-      name: "JBL FLIP 4",
+      name: "Prepper-Box",
       imageUrl:
-        "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+        "https://images.unsplash.com/photo-1493244040629-496f6d136cc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1149&q=80",
       description:
-        "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-      price: 140,
+        "We know, you probably already have it all, but wouldnt it be nice to gift yourself something random every month just in-case you could be missing something for your next adventure? We have you covered with this assorted prepper-box!.",
+      price: 125,
       countInStock: 10,
     },
   ];
